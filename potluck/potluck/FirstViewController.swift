f35bf06d6652b5b158acd5b31033b0f5a8b58fce
Vedi Chaudhri <vedichaudhri@gmail.com>
2019-11-06 07:50:03 -0800

    base app creation

---
+++ potluck/potluck/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  potluck
+//
+//  Created by Vedi Chaudhri on 11/6/19.
+//  Copyright © 2019 Vedi Chaudhri. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ potluck/potluck/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  potluck
+//
+//  Created by Vedi Chaudhri on 11/6/19.
+//  Copyright © 2019 Vedi Chaudhri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
