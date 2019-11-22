0efaf74f08a69780c1529735301b673c8bb5949d
Victoria Ding <victoriading19@gmail.com>
2019-11-21 16:49:25 -0800

    onboarding screens

--- potluck/potluck/MyCharacterButtonsViewController.swift
+++ potluck/potluck/MyCharacterButtonsViewController.swift
@@ -22,11 +22,18 @@ class MyCharacterButtonsViewController: UIViewController {
 
         if char1Info["created"]as?Bool == false {
                 //segue to create char screen & pass buttonStates value
-                performSegue(withIdentifier: "createCharacter1", sender: nil)
+                //performSegue(withIdentifier: "createCharacter1", sender: nil)
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "createCharacter") as! CreateCharacterViewController
+            newVC.charToCreate = "char1"
+            self.show(newVC, sender: self)
+            //"create character"
                 
         } else {
                 //segue to char page
-                performSegue(withIdentifier: "char1Profile", sender: nil)
+                //performSegue(withIdentifier: "char1Profile", sender: nil)
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "characterProfile") as! CharProfileViewController
+            newVC.charID = "char1"
+            self.show(newVC, sender: self)
         }
         
     }
@@ -34,22 +41,35 @@ class MyCharacterButtonsViewController: UIViewController {
     @IBAction func character2Pressed(_ sender: Any) {
         if char2Info["created"]as?Bool == false {
             //segue to create char screen & pass buttonStates value
-            performSegue(withIdentifier: "createCharacter2", sender: nil)
+            //performSegue(withIdentifier: "createCharacter2", sender: nil)
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "createCharacter") as! CreateCharacterViewController
+            newVC.charToCreate = "char2"
+            self.show(newVC, sender: self)
             
         } else {
             //segue to char page
-            performSegue(withIdentifier: "char2Profile", sender: nil)
+            //performSegue(withIdentifier: "char2Profile", sender: nil)
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "characterProfile") as! CharProfileViewController
+            newVC.charID = "char3"
+            self.show(newVC, sender: self)
         }
     }
     
     @IBAction func character3Pressed(_ sender: Any) {
         if char3Info["created"]as?Bool == false {
             //segue to create char screen & pass buttonStates value
-            performSegue(withIdentifier: "createCharacter3", sender: nil)
+            //performSegue(withIdentifier: "createCharacter3", sender: nil)
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "createCharacter") as! CreateCharacterViewController
+            newVC.charToCreate = "char3"
+            self.show(newVC, sender: self)
             
         } else {
             //segue to char page
-            performSegue(withIdentifier: "char3Profile", sender: nil)
+            //performSegue(withIdentifier: "char3Profile", sender: nil)
+            //"characterProfile"
+            let newVC = self.storyboard?.instantiateViewController(withIdentifier: "characterProfile") as! CharProfileViewController
+            newVC.charID = "char3"
+            self.show(newVC, sender: self)
         }
     }
     /*
@@ -76,21 +96,32 @@ class MyCharacterButtonsViewController: UIViewController {
         }
     }
     
+
+    
     override func viewDidAppear(_ animated: Bool) {
         char1Info =  defaults.object(forKey: "char1") as! [String : Any]
         char2Info =  defaults.object(forKey: "char2") as! [String : Any]
         char3Info =  defaults.object(forKey: "char3") as! [String : Any]
+        if char1Info["created"]as?Bool == true {
+            character1.setImage( UIImage.init(named: "SarahLamAvatar"), for: .normal)
+        }
+        if char2Info["created"]as?Bool == true {
+            character2.setImage( UIImage.init(named: "ZariaJonesAvatar"), for: .normal)
+        }
+        if char3Info["created"]as?Bool == true {
+            character3.setImage( UIImage.init(named: "SergioGarciaAvatar"), for: .normal)
+        }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nextVC = segue.destination as? CreateCharacterViewController {
             if segue.identifier == "createCharacter1" {
-                nextVC.charToCreate = "char1"
+                //nextVC.charToCreate = "char1"
             } else if segue.identifier == "createCharacter2" {
                 nextVC.charToCreate = "char2"
             } else {
                 nextVC.charToCreate = "char3"
             }
         }
-    }
+    }*/
 }
