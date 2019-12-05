539eb7b9c282e1012379384f87a089645c1aa4a8
Victoria Ding <victoriading19@gmail.com>
2019-12-04 19:05:03 -0800

    fill in feed section

--- potluck/potluck/CharactersYouFollowStoryView/FollowingStoriesViewController.swift
+++ potluck/potluck/CharactersYouFollowStoryView/FollowingStoriesViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class FollowingStoriesTableViewCell: UITableViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var charName: UILabel!
+    
+    @IBOutlet weak var storyTitle: UILabel!
+    
+    @IBOutlet weak var storyText: UILabel!
+    
+    @IBOutlet weak var charImage: UIImageView!
+    
+    @IBOutlet weak var storyImage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,19 +35,27 @@ class FollowingStoriesTableViewCell: UITableViewCell {
 
 class FollowingStoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    
+    let charNames: [String] = ["Nelson", "Mary", "George"]
+    let charImages: [String] = ["NelsonSandersAvatar", "MaryYuAvatar", "GeorgeTubbsAvatar"]
+    let storyTitles: [String] = ["Made time to prioritize myself", "Finding affordable housing in new city", "Getting ready for the race of a lifetime"]
+    let storyTexts: [String] = ["I think a common pitfall of the immigrant experience is that people work so incredibly hard to fit in, but then overlook themselves...", "I recently moved to a new city to be closer to my parents, who are elderly and speak limited English...", "Being an athlete certainly hasn't been easy. After moving to a big city, I've been having a harder time finding places to train..."]
+    let storyImages: [String] = ["Character1StoryPic1", "apartment", "Character10StoryPic1"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingStoriesTableViewCell") as! FollowingStoriesTableViewCell
-        cell.titleLabel.text = "TEST!!"
+        cell.charName.text = charNames[indexPath.section]
+        cell.charImage.image = UIImage.init(named: charImages[indexPath.section])
+        cell.storyTitle.text = storyTitles[indexPath.section]
+        cell.storyText.text = storyTexts[indexPath.section]
+        cell.storyImage.image = UIImage.init(named: storyImages[indexPath.section])
         return cell
     }
     
